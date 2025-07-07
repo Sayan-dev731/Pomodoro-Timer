bfdb34b2b5e79a19780cd0e1a50ee60516f94206
Sayan-dev731 <sayancodder731@gmail.com>
2025-07-07 20:58:56 +0530

    update and fixed in helper.ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -37,7 +37,7 @@ export function generateId(): string {
     return crypto.randomUUID();
 }
 
-export function debounce<T extends (...args: any[]) => any>(
+export function debounce<T extends (...args: unknown[]) => unknown>(
     func: T,
     wait: number
 ): (...args: Parameters<T>) => void {
@@ -48,7 +48,7 @@ export function debounce<T extends (...args: any[]) => any>(
     };
 }
 
-export function throttle<T extends (...args: any[]) => any>(
+export function throttle<T extends (...args: unknown[]) => unknown>(
     func: T,
     limit: number
 ): (...args: Parameters<T>) => void {
